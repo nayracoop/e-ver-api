b54c7ca4b4b43f023c71deeb3970b07762b0b055
Pehuén <pehuen.rodriguez@nayra.coop>
2020-11-09 16:10:06 -0300

    Version supported by ElixirLS

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EVerApi.MixProject do
     [
       app: :e_ver_api,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
