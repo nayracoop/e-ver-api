e79764893ce94d8b3bbf6bc66d3a0d5adcea2882
Pehuén <pehuen.rodriguez@nayra.coop>
2021-01-08 12:49:08 -0300

    #EVER-83 - Format

--- test/e_ver_api/accounts_test.exs
+++ test/e_ver_api/accounts_test.exs
@@ -48,60 +48,68 @@ defmodule EVerApi.AccountsTest do
     test "list_users/0 returns all users" do
       user = user_fixture()
       [listed_user] = Accounts.list_users()
+
       assert %{
-        email: "test.kinga@nayra.coop",
-        first_name: "mrs test",
-        last_name: "kinga",
-        username: "test_kinga",
-        organization: "nayracoop",
-        password_hash: hash
-      } = listed_user
+               email: "test.kinga@nayra.coop",
+               first_name: "mrs test",
+               last_name: "kinga",
+               username: "test_kinga",
+               organization: "nayracoop",
+               password_hash: hash
+             } = listed_user
+
       # check hashed pass
       assert {:ok, _} = Bcrypt.check_pass(listed_user, @password)
     end
 
     @tag individual_test: "list_users_empty"
     test "list_users/0 returns empty list if there is no users" do
-      #user = user_fixture()
+      # user = user_fixture()
       assert [] == Accounts.list_users()
     end
 
     @tag individual_test: "get_user"
     test "get_user/1 returns the user with given id" do
       user = user_fixture()
+
       assert %{
-        email: "test.kinga@nayra.coop",
-        first_name: "mrs test",
-        last_name: "kinga",
-        username: "test_kinga",
-        organization: "nayracoop"
-      } = Accounts.get_user(user.id)
+               email: "test.kinga@nayra.coop",
+               first_name: "mrs test",
+               last_name: "kinga",
+               username: "test_kinga",
+               organization: "nayracoop"
+             } = Accounts.get_user(user.id)
+
       assert Accounts.get_user(-1) == nil
     end
 
     @tag individual_test: "get_user"
     test "get_user!/1 returns the user with given id" do
       user = user_fixture()
+
       assert %{
-        email: "test.kinga@nayra.coop",
-        first_name: "mrs test",
-        last_name: "kinga",
-        username: "test_kinga",
-        organization: "nayracoop"
-      } = Accounts.get_user!(user.id)
+               email: "test.kinga@nayra.coop",
+               first_name: "mrs test",
+               last_name: "kinga",
+               username: "test_kinga",
+               organization: "nayracoop"
+             } = Accounts.get_user!(user.id)
+
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(-1) end
     end
 
     @tag individual_test: "create_user"
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
+
       assert %{
-        email: "test.kinga@nayra.coop",
-        first_name: "mrs test",
-        last_name: "kinga",
-        username: "test_kinga",
-        organization: "nayracoop"
-      } = user
+               email: "test.kinga@nayra.coop",
+               first_name: "mrs test",
+               last_name: "kinga",
+               username: "test_kinga",
+               organization: "nayracoop"
+             } = user
+
       # check hashed pass
       assert {:ok, _} = Bcrypt.check_pass(user, @password)
     end
@@ -114,15 +122,19 @@ defmodule EVerApi.AccountsTest do
     test "create_user/1 with not unique data returns error changeset" do
       # email must be unique
       user = user_fixture()
-      assert {:error, %Ecto.Changeset{action: :insert, errors: [err]}} = Accounts.create_user(@valid_attrs)
+
+      assert {:error, %Ecto.Changeset{action: :insert, errors: [err]}} =
+               Accounts.create_user(@valid_attrs)
+
       assert {:email, _} = err
       # username must be unique
-      assert {:error, %Ecto.Changeset{action: :insert, errors: [err]}} = Accounts.create_user(Map.put(@valid_attrs, :email, "changed_email@nayra.coop"))
-      assert {:username, _} = err
+      assert {:error, %Ecto.Changeset{action: :insert, errors: [err]}} =
+               Accounts.create_user(Map.put(@valid_attrs, :email, "changed_email@nayra.coop"))
 
+      assert {:username, _} = err
     end
 
-    #@tag individual_test: "update_user"
+    # @tag individual_test: "update_user"
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
@@ -135,16 +147,23 @@ defmodule EVerApi.AccountsTest do
       user = user_fixture()
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
       user_ = Accounts.get_user!(user.id)
+
       assert %{
-        first_name: "mrs test",
-        last_name: "kinga"
-      } = user_
+               first_name: "mrs test",
+               last_name: "kinga"
+             } = user_
     end
 
     @tag individual_test: "update_user"
     test "update_user/2 should not change password" do
       user = user_fixture()
-      assert {:ok, %User{} = user} = Accounts.update_user(user, %{password: "holaholu", password_confirmation: "holaholu"})
+
+      assert {:ok, %User{} = user} =
+               Accounts.update_user(user, %{
+                 password: "holaholu",
+                 password_confirmation: "holaholu"
+               })
+
       # get fresh updated user without virtuals
       user = Accounts.get_user(user.id)
       assert {:ok, _} = Bcrypt.check_pass(user, @password)
@@ -162,7 +181,6 @@ defmodule EVerApi.AccountsTest do
       del_user = Repo.get(User, user.id)
       assert del_user != nil
       assert del_user.deleted_at != nil
-
     end
 
     test "change_user/1 returns a user changeset" do
